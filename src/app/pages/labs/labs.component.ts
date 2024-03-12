8440d35e26c76404d6369b76a3e75d0b1133bfc8
Fenix2902 <alexandercalleagudelo@gmail.com>
2024-03-11 19:21:15 -0500

    11/03/2024 19:20 Se practica el Signal y continuacion con eventos

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common'
 
 @Component({
@@ -15,7 +15,7 @@ export class LabsComponent {
     'Crear Proyecto',
     'Crear componentes',
   ];
-  name = 'Alexander'
+  name = signal("Alexander")
   age = 36;
   disable = true
   img = 'https://w3schools.com/howto/img_avatar.png'
@@ -32,6 +32,13 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event) {
-console.log(event);
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event:KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value)
   }
 }
